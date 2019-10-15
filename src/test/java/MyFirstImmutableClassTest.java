7214d702588ee7c61e65edeb8ca9a5d98e12a02b
Nicolae Caprarescu <nicolae.caprarescu@gmail.com>
2019-10-15 14:59:09 +0300

    First take at defining tasks and tests

---
+++ src/test/java/MyFirstImmutableClassTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstImmutableClassTest {
+
+    @Test
+    public void shouldHaveCreatedPublicFinalClassWithoutAnyPackage() {
+
+    }
+
+    @Test
+    public void shouldHaveAddedPrivateFinalIntFieldToTheClass() {
+
+    }
+
+    @Test
+    public void shouldHaveCreatedGetterMethodForTheField() {
+
+    }
+
+    @Test
+    public void shouldHaveCreatedConstructor() {
+
+    }
+}
---
+++ tasks.md
@@ -0,0 +1,11 @@
+# Task 1
+Without specifying an explicit package, create a *public final class* called MyFirstImmutableClass
+
+# Task 2
+Create a *private final* field of type *int* called **myFirstField**
+
+# Task 3
+Create a getter method for **myFirstField**
+
+# Task 4
+Create a constructor for this class
